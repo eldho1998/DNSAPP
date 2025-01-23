a462e34ab2fdfdda1659f38cc9b2fabc2961418b
Eldhose Kurian <eldhosekurian9809@gmail.com>
2025-01-23 17:55:37 +0530

    fix

--- client/src/NavBar/navbar.jsx
+++ client/src/NavBar/navbar.jsx
@@ -1,31 +1,31 @@
 import "./navbar.css";
-import { Button, ConfigProvider, Flex, Popover } from "antd";
+// import { Button, ConfigProvider, Flex, Popover } from "antd";
 
 const NavBar = () => {
   return (
     <div className="navbar-main">
-      <ConfigProvider
+      {/* <ConfigProvider
         button={{
           style: {
             width: buttonWidth,
             margin: 4,
           },
         }}
-      >
-        <div className="logos">
-          <div className="nav-logo">
-            <img src="Logo.png" />
-          </div>
-          <div className="nav-logo2"></div>
+      > */}
+      <div className="logos">
+        <div className="nav-logo">
+          <img src="Logo.png" />
         </div>
+        <div className="nav-logo2"></div>
+      </div>
 
-        <div className="nav-contents">
-          <p>HOME</p>
-          <p>MENU</p>
-          <p>MAKE A RESERVETION</p>
-          <p>CONTACT US</p>
-        </div>
-      </ConfigProvider>
+      <div className="nav-contents">
+        <p>HOME</p>
+        <p>MENU</p>
+        <p>MAKE A RESERVETION</p>
+        <p>CONTACT US</p>
+      </div>
+      {/* </ConfigProvider> */}
     </div>
   );
 };
